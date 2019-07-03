5fa64e5c98496b4da39fd7eecf155ea06303636d
lj <lslj0516@163.com>
2019-07-03 17:34:20 +0800

    add: 注册协议

--- ha_service.go
+++ ha_service.go
@@ -3,6 +3,8 @@ package bulbasaur
 import (
 	"context"
 
+	"github.com/pkg/errors"
+
 	empty "github.com/golang/protobuf/ptypes/empty"
 	pb "github.com/lj-211/bulbasaur/protocol"
 )
@@ -12,4 +14,25 @@ type HaServer struct{}
 func (s *HaServer) HeartBeat(ctx context.Context, req *pb.HeartBeatReq) (*empty.Empty, error) {
 	Logger.Info("SER-收到心跳包: ", req.Id)
 	return &empty.Empty{}, nil
+}
+
+func (s *HaServer) GetNodeList(ctx context.Context, req *pb.GetNodeListReq) (*pb.GetNodeListRes, error) {
+	return nil, errors.New("not implemented")
+}
+
+func (s *HaServer) Register(ctx context.Context, req *pb.RegisterReq) (*pb.RegisterRes, error) {
+	// 1. 检查节点是否存在
+	if v, ok := bulbasaur.Partners[req.Id]; ok {
+		v.LastActive = time.Now()
+		common.Log.Warningf("parterner-%+v已注册，重复收到注册消息", req.Id)
+	} else {
+		bulbasaur.Partners[req.Id] = &bulbasaur.NodeInfo{
+			NodeId:     req.Id,
+			Addr:       req.Addr,
+			LastActive: time.Now(),
+			//Role: 1,
+		}
+		common.Log.Infof("新的parterner-%+v加入", req.Id)
+	}
+	return nil, errors.New("not implemented")
 }
--- node_info.go
+++ node_info.go
@@ -1,12 +1,24 @@
 package bulbasaur
 
 import (
+	"math/rand"
+	//"sync"
 	"time"
+
+	pb "github.com/lj-211/bulbasaur/protocol"
 )
 
 type NodeInfo struct {
-	NodeId     uint32
+	NodeId     uint64
 	Addr       string // 暂时没有服务发现，使用ip:port
 	LastActive time.Time
-	Role       int32
+	Role       pb.Role
 }
+
+type NodeMap map[uint64]*NodeInfo
+
+var Info *NodeInfo = &NodeInfo{
+	NodeId: uint64(rand.Uint32() + 1),
+}
+
+var Partners NodeMap = make(NodeMap)
